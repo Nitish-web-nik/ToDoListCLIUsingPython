a4143f3d9e423c5a7e21c5c20db64a5ac2586a18
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-08 23:01:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+
+
+def main():
+    print('Started the to do application.....')
+    while (1):
+        command = input('$ ')
+        if(command == 'quit'):
+            break
+
+if __name__ == '__main__':
+    main()
